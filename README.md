fbf92c90bfe83aba3eba4dc5709c314b83164b71
kohutdb <davyd.kohut.business@gmail.com>
2024-09-25 23:03:57 +0300

    init

---
+++ README.md
@@ -0,0 +1,187 @@
+# jepc
+
+Fully featured JSON-RPC handler for JavaScript/Node.js.
+If you need a server, try [sepc](https://github.com/kohutd/sepc).
+If you need a client, try [repc](https://github.com/kohutd/repc).
+If you need details, read [JSON-RPC 2.0 Specification](https://www.jsonrpc.org/specification).
+
+## Installation
+
+```shell
+npm i jepc
+```
+
+## Usage
+
+Basic:
+
+```javascript
+import jepc from 'jepc';
+
+const add = (a, b) => a + b;
+const ping = () => null;
+
+const { handle } = jepc({ add, ping });
+
+const result4 = await handle({ jsonrpc: '2.0', method: 'add', params: [2, 2], id: 1 });
+const result8 = await handle('{ "jsonrpc": "2.0", "method": "add", "params": [4, 4], "id": 2 }');
+const notificationResult = await handle({ jsonrpc: '2.0', method: 'ping' });
+const batchResult = await handle([
+    { jsonrpc: '2.0', method: 'add', params: [2, 2], id: 1 },
+    { jsonrpc: '2.0', method: 'add', params: [4, 4], id: 2 },
+    { jsonrpc: '2.0', method: 'ping' },
+]);
+
+console.log(result4); // { jsonrpc: '2.0', result: 4, id: 1 }
+console.log(result8); // { jsonrpc: '2.0', result: 8, id: 2 }
+console.log(notificationResult); // undefined
+console.log(batchResult); // [{ jsonrpc: '2.0', result: 4, id: 1 }, { jsonrpc: '2.0', result: 8, id: 2 }]
+
+```
+
+Express:
+
+```javascript
+import jepc from 'jepc';
+import express from 'express';
+
+const add = (a, b) => a + b;
+
+const { handle } = jepc({ add });
+
+const app = express();
+
+app.use(express.text({ type: '*/*' }));
+
+app.post('/', (req, res) => {
+    handle(req.body)
+        .then((out) => res.send(out));
+});
+```
+
+Accessing params:
+
+```javascript
+// natural (like general js function) 
+function add(a, b) {
+    return a + b;
+}
+
+// using destruction
+function createUser({ name, email }) {
+    const user = { /* handle user creation */ };
+
+    return user;
+}
+
+// using this.params
+function createUser() {
+    const { name, email } = this.params;
+
+    const user = { /* handle user creation */ };
+
+    return user;
+}
+
+// using ...
+function sort(...numbers) {
+    return numbers.sort();
+}
+
+// using "arguments"
+// parameter names must be empty here
+function sum() {
+    const numbers = arguments[0];
+
+    return Object.values(numbers).reduce((s, c) => s + c, 0);
+}
+```
+
+Method class:
+
+```javascript
+import jepc, { Method } from 'jepc';
+
+class SumMethod extends Method {
+    handle(params) {
+        return Object.values(params)
+            .reduce((s, c) => s + c, 0);
+    }
+}
+
+class ReturnId extends Method {
+    handle(params, context) {
+        return context.request.id;
+    }
+}
+
+const sum = new SumMethod();
+
+const { handle } = jepc({ sum });
+```
+
+Producing errors:
+
+```javascript
+import jepc, { JsonRpcError } from 'jepc';
+
+function divide(a, b) {
+    if (b === 0) {
+        throw new JsonRpcError(-32602, 'Cannot divide by zero');
+    }
+
+    return a / b;
+}
+
+jepc({ divide });
+```
+
+Handling errors:
+
+```javascript
+import jepc from 'jepc';
+
+const add = () => {
+    throw new Error('Unsupported!');
+}
+
+const { setErrorHandler } = jepc({ divide });
+
+setErrorHandler((error, context, defaultErrorHandler) => {
+    console.error(error);
+
+    return defaultErrorHandler(error, context);
+});
+```
+
+## API
+
+### `handle`
+
+Handle JSON-RPC request. Supports object, array and string as input.
+If string is passed, it will be parsed as JSON.
+
+- type: `function(object | array | string)`
+- result: `Promise<object | array | undefined>`
+- example:
+
+```javascript
+handle('{}'); // invalid request error
+handle('[]'); // invalid request error
+handle('a'); // parse error
+handle({ "jsonrpc": "2.0", "method": "add", "params": [4, 4], "id": 2 }); // ok
+handle('{ "jsonrpc": "2.0", "method": "ping" }'); // ok, undefined returned
+handle([{ "jsonrpc": "2.0", "method": "add", "params": [4, 4], "id": 2 }]) // ok, array returned
+```
+
+### `methods`
+
+Available methods.
+
+- type: `Record<string, function>`
+
+### `setErrorHandler`
+
+Set error handler.
+
+- type: `function(function(error, context, defaultErrorHandler))`
---
+++ src/main.js
@@ -0,0 +1,216 @@
+import getParameterNames from "get-parameter-names";
+
+export class JsonRpcError {
+    constructor(code, message, data = undefined) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+}
+
+export class Method {
+    handle(params) {
+    }
+}
+
+class FnMethod extends Method {
+    constructor(fn) {
+        super();
+
+        this.fn = fn;
+        this.parameterNames = getParameterNames(fn);
+    }
+
+    handle(params, context) {
+        const thisContext = { params, context };
+
+        const names = this.parameterNames;
+
+        if (!names.length || names[0].startsWith('{') || names[0].startsWith('[')) {
+            return this.fn.call(thisContext, params);
+        }
+
+        if (!Array.isArray(params) && !names[0].startsWith('...')) {
+            params = names.map((name) => params[name]);
+        }
+
+        return this.fn.call(thisContext, ...params);
+    }
+}
+
+function makeError(id, error) {
+    return {
+        jsonrpc: '2.0',
+        error,
+        id,
+    };
+}
+
+function makeResult(id, result) {
+    return {
+        jsonrpc: '2.0',
+        result,
+        id,
+    };
+}
+
+function jepc(methods = {}) {
+    methods = { ...methods };
+
+    Object.entries(methods).forEach(([name, fn]) => {
+        if (fn instanceof Method) {
+            methods[name] = fn;
+        } else {
+            methods[name] = new FnMethod(fn);
+        }
+    });
+
+    const defaultErrorHandler = (error, context) => {
+        const { id } = context.request;
+
+        if (!id) {
+            return '';
+        }
+
+        if (error instanceof JsonRpcError) {
+            return makeError(id, {
+                code: error.code,
+                message: error.message,
+                data: error.data,
+            });
+        } else {
+            console.log(error);
+
+            return makeError(id, {
+                code: -32603,
+                message: 'Internal error',
+            });
+        }
+    }
+    let errorHandler = defaultErrorHandler;
+
+    function setErrorHandler(handler) {
+        errorHandler = handler;
+    }
+
+    async function handleSingle(request, context = {}) {
+        // { "jsonrpc": "2.0", "method": "...", "params": [], "id": 1 }
+        if (!request || typeof request !== 'object') {
+            return makeError(null, {
+                code: -32600,
+                message: 'Invalid Request',
+            });
+        }
+
+        context.request = request;
+
+        let { jsonrpc, method, params, id = null } = request;
+
+        if (params == null) {
+            params = [];
+        }
+
+        // 2.0
+        if (jsonrpc !== '2.0') {
+            return makeError(id, {
+                code: -32600,
+                message: 'Invalid Request',
+            });
+        }
+
+        // "getUsers"
+        if (typeof method !== 'string') {
+            return makeError(id, {
+                code: -32600,
+                message: 'Invalid Request',
+            });
+        }
+
+        // {}, []
+        if (!params || typeof request !== 'object') {
+            return makeError(id, {
+                code: -32600,
+                message: 'Invalid Request',
+            });
+        }
+
+        // 1, 3.14, "ha.sh"
+        if (id && typeof id !== "string" && typeof id !== "number") {
+            return makeError(id, {
+                code: -32600,
+                message: 'Invalid Request',
+            });
+        }
+
+        if (!(method in methods)) {
+            return makeError(id, {
+                code: -32601,
+                message: 'Method not found'
+            });
+        }
+
+        try {
+            const result = await methods[method].handle(params, {
+                ...context,
+                request,
+            });
+
+            const madeResult = makeResult(id, result);
+
+            if (!id) {
+                return '';
+            }
+
+            return madeResult;
+        } catch (e) {
+            return errorHandler(e, context, defaultErrorHandler);
+        }
+    }
+
+    async function handle(message, context = {}) {
+        let request;
+
+        if (typeof message === 'string') {
+            try {
+                request = JSON.parse(message);
+            } catch (e) {
+                return makeError(null, {
+                    code: -32700,
+                    message: 'Parse error'
+                });
+            }
+        } else {
+            request = message;
+        }
+
+        let output;
+
+        if (Array.isArray(request)) {
+            if (!request.length) {
+                return makeError(null, {
+                    code: -32600,
+                    message: 'Invalid Request',
+                });
+            }
+
+            output = (await Promise.all(request.map((request) => handleSingle(request, context))))
+                .filter((v) => !!v);
+
+            if (!output.length) {
+                return undefined;
+            }
+        } else {
+            output = await handleSingle(request, context);
+        }
+
+        if (output) {
+            return output;
+        } else {
+            return undefined;
+        }
+    }
+
+    return { methods, handle, setErrorHandler };
+}
+
+export default jepc;
